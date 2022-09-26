d88fb07a671b66b6103aaf77b0c6b829eac00b62
Daniel Whitcomb <danrwhitcomb@gmail.com>
2022-09-25 23:06:16 -0400

    micronaut and risks

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 hugo -D
-aws s3 sync ./public s3://danwhitcomb-com --acl public-read
+aws s3 sync ./public s3://danwhitcomb-com --acl public-read --delete --cache-control max-age=43200
