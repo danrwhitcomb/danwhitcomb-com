427f45686c190047df58763a5cf7f3349e4a9a50
danwhitcomb-toast <dan.whitcomb@toasttab.com>
2024-07-03 09:52:34 -0400

    GA

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,5 @@
 
 hugo -D
 aws s3 sync ./public s3://danwhitcomb-com --acl public-read --delete --cache-control max-age=43200
+
+aws cloudfront create-invalidation --distribution-id E2MR3EZL7U5COV --paths "/*"
